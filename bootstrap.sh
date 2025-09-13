289877cb1ac45ad8bbf18f90c73e6bd65e1f4d4c
akhil27051999 <thyadiakhil@gmail.com>
2025-09-13 08:56:57 +0000

    Merge dev into main, resolved bootstrap.sh conflicts

--- bootstrap.sh
+++ bootstrap.sh
@@ -83,10 +83,6 @@ main() {
     install_postgres_client
     install_docker
     install_kubernetes_tools
-<<<<<<< HEAD
-=======
-
->>>>>>> dev
     log "Bootstrap completed!"
     log "Please log out and log back in (or run 'newgrp docker') to use Docker without sudo."
     log "Run 'minikube start --driver=docker --nodes 3' to start your Kubernetes cluster."
