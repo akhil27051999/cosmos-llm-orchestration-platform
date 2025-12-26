c9a78ee22a93a4b3faff916e6a1551207b2e16d2
akhil27051999 <thyadiakhil@gmail.com>
2025-12-26 01:30:06 +0000

    updated terraform varaiables with AWS resources for cloud migration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,47 +1,58 @@
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "project_name" {
-  description = "Project name for resource naming"
-  type        = string
-  default     = "api-server"
-}
-
-variable "ami_id" {
-  description = "Ubuntu AMI ID"
-  type        = string
-  default     = "ami-020cba7c55df1f615"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.xlarge"
-}
-
-variable "key_name" {
-  description = "Key pair name"
-  type        = string
-  default     = "api-server"
+  default = "two-az-network"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-  description = "Public subnet CIDR block"
-  type        = string
-  default     = "10.0.1.0/24"
+variable "azs" {
+  type = map(object({
+    public        = string
+    app           = string
+    db            = string
+    dependent     = string
+    observability = string
+  }))
+  default = {
+    "us-east-1a" = {
+      public        = "10.0.0.0/24"
+      app           = "10.0.1.0/24"
+      db            = "10.0.2.0/24"
+      dependent     = "10.0.3.0/24"
+      observability = "10.0.4.0/24"
+    }
+    "us-east-1b" = {
+      public        = "10.0.10.0/24"
+      app           = "10.0.11.0/24"
+      db            = "10.0.12.0/24"
+      dependent     = "10.0.13.0/24"
+      observability = "10.0.14.0/24"
+    }
+  }
 }
 
-variable "availability_zone" {
-  description = "Availability zone"
-  type        = string
-  default     = "us-east-1a"
+variable "ami_id" {
+  default = "ami-020cba7c55df1f615"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+}
+
+variable "my_ip" {
+  description = "Your public IP for SSH access"
+
+  validation {
+    condition     = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/32$", var.my_ip))
+    error_message = "my_ip must be a valid /32 CIDR, e.g., 203.0.113.25/32"
+  }
 }
